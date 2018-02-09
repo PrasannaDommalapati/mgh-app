d3082ca8d60ee113e47c11dc317649420d40a95a
Prasanna Kumar <prasanna.kumar@headforwards.com>
2018-02-09 22:31:57 +0000

    video added

--- src/app/components/dashboard/component.ts
+++ src/app/components/dashboard/component.ts
@@ -1,20 +1,49 @@
 import {Component} from '@angular/core';
-import {UserService} from '../../../modules/user/services/user-api';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component(
     {
-        selector:    'app-dashboard',
+        selector: 'app-dashboard',
         templateUrl: 'component.html',
-        styleUrls:   ['component.scss'],
+        styleUrls: ['component.scss'],
     })
 export class Dashboard {
 
-    public constructor(private UserService: UserService) {
+
+    public videoList: any = [
+        {
+            name: "video1",
+            slug: "video1",
+            embed: "6wD4V0rvlDI"
+        },
+        {
+            name: "video2",
+            slug: "video2",
+            embed: "NFexAcg8PRw"
+        },
+        {
+            name: "video3",
+            slug: "video3",
+            embed: "7iCDRJ1tD1I"
+        },
+        {
+            name: "video4",
+            slug: "video4",
+            embed: "1FN2VZ2DWYI"
+        },
+        {
+            name: "video5",
+            slug: "video5",
+            embed: "NFexAcg8PRw"
+        }];
+
+    public constructor(private sanitizer: DomSanitizer,) {
     }
 
-    public inGroup(group: string) {
 
-        return this.UserService.isInGroup(group);
+    public getEmbedUrl(item: any) {
+
+        return this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/" + item.embed);
     }
 
 }
