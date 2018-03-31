6032b756aaa98fa8bd14aedb92439a224c1441f0
Prasanna Kumar <prasanna.kumar@headforwards.com>
2018-03-31 10:57:57 +0100

    merge

--- src/app/environment/production.ts
+++ src/app/environment/production.ts
@@ -8,6 +8,6 @@ export const envParams = {
         identityPoolId: 'eu-west-2:b22ece19-4e83-4ff9-a460-b3abb3b2c26d'
     },
     endpoints: {
-        user:    'https://8fu9z1nq2j.execute-api.eu-west-2.amazonaws.com/production/',
+        user: 'https://s6kaxe59ph.execute-api.eu-central-1.amazonaws.com/local/'
     },
 };
